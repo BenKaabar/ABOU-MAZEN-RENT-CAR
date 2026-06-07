afe917bc900265961cf35eaa581b8545c7b01eda
BenKaabar <achraf.kaabar@gmail.com>
2026-06-07 20:05:11 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ABOU_MAZEN_RENT_CAR';
+}
