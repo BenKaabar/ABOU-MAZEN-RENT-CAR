a2d11e0ea6b4e7dfa1d61a04b2609bfea79e8ebf
BenKaabar <achraf.kaabar@gmail.com>
2026-06-24 11:35:44 +0100

    feat service component with animation and add icon images

--- src/app/Components/Services/services/services.component.ts
+++ src/app/Components/Services/services/services.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
-export class ServicesComponent {
+export class ServicesComponent implements AfterViewInit {
+
+  @ViewChildren('animEl') cards!: QueryList<ElementRef>;
+
+  // ********************************************************************** Page Animation **********************************************************************
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+
+            // Animate only once
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        threshold: 0.2
+      }
+    );
+
+    this.cards.forEach((card) => {
+      observer.observe(card.nativeElement);
+    });
+  }
 
 }
